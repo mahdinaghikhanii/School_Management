552e3d7b0a07c341583ef29823f876672340172f
Mahdinaghikhani <mahdinkh7@gmail.com>
2022-05-11 14:36:49 +0430

    debugs in sidbar

--- lib/screan/dashbord/dashbord.dart
+++ lib/screan/dashbord/dashbord.dart
@@ -1,24 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:schoolmanagement/module/widgets.dart';
 
 import 'sidbar.dart';
 import 'usereinfo.dart';
 import 'package:schoolmanagement/module/extension.dart';
+
+MBloc<int> _menu = MBloc<int>()..setValue(1);
 
 class Dashboard extends StatelessWidget {
   const Dashboard({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: SafeArea(
-            child: Row(
-      children: [
-        SidBar(),
-        Container(
-          color: Colors.blue.shade100,
-        ).expanded,
-        UserInfo()
-      ],
-    )));
+    return StreamBuilder<int>(
+        stream: _menu.stream,
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return const MWaiting();
+          }
+          return Scaffold(
+              body: SafeArea(
+                  child: Row(
+            children: [
+              SidBar(
+                onChanged: (val) => _menu.setValue(val),
+                selectIndex: _menu.value,
+              ),
+              Container(
+                color: Colors.blue.shade100,
+              ).expanded,
+              UserInfo()
+            ],
+          )));
+        });
   }
 }
--- lib/screan/dashbord/sidbar.dart
+++ lib/screan/dashbord/sidbar.dart
@@ -4,60 +4,56 @@ import '../../module/extension.dart';
 import '../../module/widgets.dart';
 
 class SidBar extends StatelessWidget {
-  const SidBar({Key? key}) : super(key: key);
+  final Function(int) onChanged;
+  final int selectIndex;
+  const SidBar({required this.onChanged, required this.selectIndex, Key? key})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    MBloc<int> _selindex = MBloc<int>()..setValue(1);
-    return StreamBuilder<int>(
-        stream: _selindex.stream,
-        builder: (context, snapshot) {
-          if (!snapshot.hasData) {
-            return Container();
-          }
-          return SizedBox(
-            width: context.widthResponse(0.23, 180, 250),
-            child: Column(
-              children: [
-                const SizedBox(
-                  height: 35,
-                ),
-                'Schole'.toLabel(fontsize: 18, color: Colors.grey.shade600),
-                const SizedBox(
-                  height: 70,
-                ),
-                MSideBarItem(
-                    selected: snapshot.data == 1,
-                    title: "Dashbord",
-                    icon: Icons.dashboard,
-                    onPreassed: () => _selindex.setValue(1)),
-                MSideBarItem(
-                    title: "Messages",
-                    value: 3,
-                    selected: snapshot.data == 2,
-                    icon: Icons.message,
-                    onPreassed: () => _selindex.setValue(2)),
-                MSideBarItem(
-                    title: "Students",
-                    selected: snapshot.data == 3,
-                    icon: Icons.people,
-                    onPreassed: () => _selindex.setValue(3)),
-                MSideBarItem(
-                    title: "Classes",
-                    icon: Icons.class_,
-                    selected: snapshot.data == 4,
-                    onPreassed: () => _selindex.setValue(4)),
-                const SizedBox(
-                  height: 50,
-                ),
-                MSideBarItem(
-                    title: "Settings",
-                    selected: snapshot.data == 5,
-                    icon: Icons.settings,
-                    onPreassed: () => _selindex.setValue(5))
-              ],
-            ),
-          );
-        });
+    return SizedBox(
+      width: context.widthResponse(0.23, 180, 250),
+      child: Column(
+        children: [
+          const SizedBox(
+            height: 35,
+          ),
+          'Schole'.toLabel(fontsize: 18, color: Colors.grey.shade600),
+          const SizedBox(
+            height: 70,
+          ),
+          MSideBarItem(
+              selected: selectIndex == 1,
+              title: "Dashbord",
+              icon: Icons.dashboard,
+              onPreassed: () => onChanged(1)),
+          MSideBarItem(
+              title: "Messages",
+              value: 2,
+              selected: selectIndex == 2,
+              icon: Icons.message,
+              onPreassed: () => onChanged(1)),
+          MSideBarItem(
+              title: "Students",
+              selected: selectIndex == 3,
+              icon: Icons.people,
+              onPreassed: () => onChanged(3)),
+          MSideBarItem(
+              title: "Classes",
+              icon: Icons.class_,
+              selected: selectIndex == 4,
+              onPreassed: () => onChanged(4)),
+          const SizedBox(
+            height: 50,
+          ),
+          MSideBarItem(
+              title: "Settings",
+              selected: selectIndex == 5,
+              icon: Icons.settings,
+              onPreassed: () => onChanged(5)),
+        ],
+      ),
+    );
+    ;
   }
 }
