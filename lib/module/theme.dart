55f2555d1c12b510f95ca5db0e06a8bb5482516d
Mahdinaghikhani <mahdinkh7@gmail.com>
2022-05-13 13:06:00 +0430

    add theme.dart

---
+++ lib/module/theme.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+enum AppTheme { dark, light }
+
+final Map<AppTheme, ThemeData> appThemeData = {
+  AppTheme.light: ThemeData(
+      brightness: Brightness.light,
+      primarySwatch: Colors.blue,
+      primaryColor: Colors.blue,
+      scaffoldBackgroundColor: Colors.white,
+      bottomAppBarColor: Colors.grey[100],
+      textTheme: TextTheme(headline1: TextStyle(color: Colors.grey[100]))),
+  AppTheme.dark: ThemeData(
+      brightness: Brightness.dark,
+      primarySwatch: Colors.blueGrey,
+      primaryColor: Colors.blueGrey,
+      scaffoldBackgroundColor: Colors.lightBlueAccent,
+      bottomAppBarColor: Colors.grey[100],
+      textTheme: const TextTheme(headline1: TextStyle(color: Colors.white70)))
+};
